39ccaf9f4ab945fa9c20ab3d19ef848e841bd119
xiaobo.kong <xiaobo.kong@aliyun.com>
2021-06-15 14:31:55 +0800

    pcm16le 左右声道分离demo

---
+++ pcm16le_split.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int simplest_pcm16le_split(char *url)
+{
+    
+
+}
+
+int main(int argc,char *argv[])
+{
+    printf("pcm16le split...\r\n");
+    simplest_pcm16le_split("");
+    return 0;
+}
