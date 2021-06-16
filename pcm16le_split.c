c5b047ae6959b3e3e1f03b7facee3c986bbbf603
xiaobo.kong <xiaobo.kong@aliyun.com>
2021-06-16 10:35:51 +0800

    完善左右声道分离demo

--- pcm16le_split.c
+++ pcm16le_split.c
@@ -1,14 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int simplest_pcm16le_split(char *url)
 {
-    
+    FILE *fp = fopen(url, "rb+");
+    FILE *fp1 = fopen("output_1.pcm", "wb+");
+    FILE *fp2 = fopen("output_r.pcm", "wb+");
+    unsigned char *sample = (unsigned char *)malloc(4);
 
+    while(!feof(fp))
+    {
+        fread(sample, 1, 4, fp);
+        //L
+        fwrite(sample,1,2,fp1);
+        //W
+        fwrite(sample+2,1,2,fp2);
+    }
+
+    free(sample);
+    fclose(fp);
+    fclose(fp1);
+    fclose(fp2);
+
+    return 0;
 }
 
 int main(int argc,char *argv[])
 {
     printf("pcm16le split...\r\n");
-    simplest_pcm16le_split("");
+    simplest_pcm16le_split("NocturneNo2inEflat_44.1k_s16le.pcm");
     return 0;
 }
