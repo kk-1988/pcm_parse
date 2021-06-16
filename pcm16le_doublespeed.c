895f6d9fbede72c475b4b09f73f6a5482cbc8361
xiaobo.kong <xiaobo.kong@aliyun.com>
2021-06-16 10:44:36 +0800

    完善声音播放速度提升一倍的demo

---
+++ pcm16le_doublespeed.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void simplest_pcm16le_doublespeed(char *url)
+{
+    
+
+}
+
+int main(int argc,char *argv[])
+{
+    printf("pcm16le doublespeed parse...\r\n");
+    return 0;
+}
