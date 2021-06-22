fe2ad753e4c7d7f29825675f65212ff340c0949d
xiaobo.kong <xiaobo.kong@aliyun.com>
2021-06-22 11:20:40 +0800

    完善pcm16le convert to pcm8的demo

--- pcm16le_to_pcm8.c
+++ pcm16le_to_pcm8.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-void simplest_pcm16le_to_pcm8(char *url)
+int simplest_pcm16le_to_pcm8(char *url)
 {
     FILE *fp = fopen(url, "rb+");
     FILE *fp1 = fopen("output_8.pcm","wb+");
@@ -20,10 +20,25 @@ void simplest_pcm16le_to_pcm8(char *url)
         //(-32768-32768)
         samplenum16 = (short *)sample;
         samplenum8 = (*samplenum16)>>8;
+        //(0-255)
         samplenum8_u = samplenum8 + 128;
-        
+        //L
+        fwrite(&samplenum8_u,1,1,fp1);
 
+        samplenum16 = (short *)(sample + 2);
+        samplenum8=(*samplenum16)>>8;
+		samplenum8_u=samplenum8+128;
+
+        //R
+        fwrite(&samplenum8_u,1,1,fp1);
+		cnt++;
     }
+    printf("Sample Cnt:%d\n",cnt);
+ 
+	free(sample);
+	fclose(fp);
+	fclose(fp1);
+	return 0;
 
 }
 
