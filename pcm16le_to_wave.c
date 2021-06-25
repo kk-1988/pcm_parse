220673b061efb7f1bfc15735d16b31e6e4a11485
xiaobo.kong <xiaobo.kong@aliyun.com>
2021-06-25 14:02:49 +0800

    pcm16le convert to wave...

---
+++ pcm16le_to_wave.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int simplest_pcm16le_to_wave(const char *pcmpath,int channels,int sample_rate,const char *wavepath)
+{
+
+
+}
+
+int main(int argc,char *argv[])
+{
+    printf("simplest_pcm16le_to_wave parse...\r\n");
+    simplest_pcm16le_to_wave();
+    return 0;
+}
