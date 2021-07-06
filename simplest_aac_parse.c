cd45f98be33182c2a8946670c66a4c169148aec2
xiaobo.kong <xiaobo.kong@aliyun.com>
2021-07-06 10:22:16 +0800

    aac解析程序demo

---
+++ simplest_aac_parse.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//aac 解析
+void simplest_aac_parse(char *url)
+{
+
+}
+
+int main(int argc,char *argv[])
+{
+    printf("simplest_aac_parse...\r\n");
+    simplest_aac_parse("");
+}
